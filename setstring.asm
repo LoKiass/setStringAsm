22a0c58ca086e2ea0e30877eb4c935dd6fb8b65b
LoKias_ <diseursmat@gmail.com>
2025-01-09 17:32:56 +0100

    Ajout de commenaitaire expliquant le fonctionnement

--- setstring.asm
+++ setstring.asm
@@ -8,72 +8,73 @@
     intro db ? 11  
     
   .code
-    call dsdata
-    lea si,mess1
-    mov di,160
-    mov ah,0F0h
-    call printf   
+    call dsdata     ; Appel du sous programme "dsdata"
+    lea si,mess1    ; Chargement de l'adresse de mess1 (string) dans le SI (source index)
+    mov di,160      ; Positionnement de DI (Position ecran) en 160 (Saut de ligne)
+    mov ah,0F0h     ; Configuration de AH (Couleur de fond + couleu du charactere) en blanc sur noir (console de base)
+    call printf     ; Appel du sous programme printf
   
-    mov cx,10
-    call dsdata
-    lea si,intro
- introcar:   
-    mov ah,0
-    int 16h
-    cmp al,13
-    je sortie
-    mov ah,0Eh
-    call dsecr
-    mov [di],ax
-    add di,2
-    call dsdata
-    mov [si],al
-    inc si
-    loop introcar 
-  sortie:
-    call dsdata
-    mov [si],0
+    mov cx,10       ; Chargement de la valeur 10 dans le registre cx (Autrement dire, le nombre maximale de charactere) 
+    call dsdata     ; Appel du sous programme "dsdata"
+    lea si,intro    ; Chargement de l'adresse de intro dans SI (Au debut rien, mais sera le retour de notre frappe plus tard)
+ 
+ introcar:          ; Outils permetant d'introduit des charactere dans un string (SI)  
+    mov ah,0        ; Initialiser la valeur de AH a 0 (Pour l'attente d'interuption)
+    int 16h         ; Attente d'interuption => (Frappe effectuer charger dans al)  
+    cmp al,13       ; Comparer la valeur sortante avec 13 (Touche enter) 
+    je sortie       ; Si comparaison correct : appel du sous programme sortie
+    mov ah,0Eh      ; Sinon, Configuration du retour ecran (jaune sur noir)
+    call dsecr      ; Appel du sous programme "dsecran" 
+    mov [di],ax     ; Chargement de ax (ah et al) dans le contenue de l'adresse di (addrese DI au debut en 162-164 apres la premiere chaine de charactere)
+    add di,2        ; Incrementation de DI de 2 (2 octect etant ah et al)
+    call dsdata     ; Appel du sous programme "dsdata" 
+    mov [si],al     ; Chargement de al (code ascii de notre frappe) dans le contenue de l'adresse de si 
+    inc si          ; Incrementation de si (Passer pour le prochain charactere que nous allons frapper)
+    loop introcar   ; Retourner vers l'outils introcar 
+  sortie:           ; Arriveras si et seulement al = 13
+    call dsdata     ; Apelle du sous programme "dsdata"
+    mov [si],0      ; Chargement de la valeur 0 dans le contenue de l'adresse si (Fin de chaine pour notre sous-prog "printf") 
      
-    call dsdata
-    lea si,mess2
-    mov di,320
-    mov ah,0Fh
-    call printf
+    call dsdata     ; Appel du sous-programme "dsdata"
+    lea si,mess2    ; Charment du string msg2 dans le l'adresse de SI
+    mov di,320      ; Configuration de l'emplacement ecran en 320 (Autre saut de ligne)
+    mov ah,0Fh      ; Configuration de AH (blanc sur noir)
+    call printf     ; Appel du sous-programme "printf"
     
-    call dsdata
-    lea si,intro
-    mov ah,0E0h
-    call printf
+    call dsdata     ; Appel du sous-programme "dsdata"
+    lea si,intro    ; Chargement de string intro dans si
+    mov ah,0E0h     ; Configuration ecran de intro (Noir sur blanc)
+    call printf     ; Appel du sous-prog "Printf"
        
-    mov ah,4Ch  ; arrêt... 
-    int 21h     ;   ...système 
+    mov ah,4Ch  
+    int 21h    
     
-    dsdata:
-        push bx
-        mov bx,@data   ;mov ds,@data impossible
-        mov ds,bx
-        pop bx 
-        ret
+    dsdata:             ; dsdata s'occupe de la gestion du segment data
+        push bx         ; Sauvegarde du registre bx dans la pile        
+        mov bx,@data    ; Chargment de l'adresse du segment data dans le registre bx (ds < data IMPOSSIBLE)
+        mov ds,bx       ; Chargement de l'adresse de bx dans le registre ds (Afin d'acceder a l'adresse de @data)
+        pop bx          ; Restauration de la valeur de bx en fonction de la pile          
+        ret             ; Retour
         
-    dsecr:
-        push bx
-        mov bx,0B800h   ;mov ds,0B800h impossible
-        mov ds,bx 
-        pop bx
-        ret
+    dsecr:              ; Gestion de l'affichage ecran
+        push bx         ; Sauvegarde du registre bx dans la pile
+        mov bx,0B800h   ; Chargment de l'adresse du segment ecran (0B800h) dans le registre bx (ds < 0B800h IMPOSSIBLE du au type d'adressage) 
+        mov ds,bx       ; Chargement de l'adresse de bx dans le registre ds 
+        pop bx          ; Restauration de la valeur de bx en fonction de la pile 
+        ret             ; Retour
     
     printf:
-        call dsdata
-        mov al,[si] 
-        cmp al,0
-        je fin
-        call dsecr
-        mov [di],ax
-        inc si
-        add di,2
-        jmp printf 
+        call dsdata     ; Appel du sous programme dsdata (Effet d'essuie glace)
+        mov al,[si]     ; Chargment du contenue de SI (1 charactere) dans le registre al (Qui complete AH, resultant en AX)   
+        cmp al,0        ; Comparaisons de AL (ou meme SI) ï¿½ la valeur 0 (Fin de la chaine de charctere)
+        je fin          ; Si vrai, appel du sous-programme fin (qui effectuer simplement un return vers le segment "code)
+        call dsecr      ; Appel du sous programme "dsecran" (Gestion de l'affichage ecran et du segment ecran)
+        mov [di],ax     ; Chargement du registre ax (ah + al) dans le contenue de l'adresse de di 
+        inc si          ; Incrementation du registre SI (+1 pour passer au prochain charactere de la chaine) 
+        add di,2        ; Double incrementation du registre DI (AH et AL considerer a ce moment comme 2 octect)
+        jmp printf      ; Saut vers le sous programme printf (Permet d'afficher la chaine de charactere au complet)
      fin: 
-        ret
+        ret             ; Cela arrivera seulement quand notre string sera entierement arriver a 0 (Fin de la chaine de charactere)
     
     
-    end         ; arrêt de la phase d'assemblage
+    end         ; arrï¿½t de la phase d'assemblage
